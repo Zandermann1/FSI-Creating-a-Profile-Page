381d13bebfd343308cf9d5c610673459e405daac
Zandermann1 <101841690+Zandermann1@users.noreply.github.com>
2022-04-05 20:59:06 -0500

    part 1 complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-let content = document.querySelector('.js-generated.content')
+letContent = document.querySelector('.js-generated.content')
+letHeader = document.createElement("h1")
+header.setAttribute('class', 'dogName')
+header.append('Rizzo')
+content.append(header)
+
+let content = document.querySelector("dog-image")
+let header = document.createElement()
